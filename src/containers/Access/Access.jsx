d5e4c10ace34f73a05d366c1b7007c0b1346ee70
Lucimux <vicente.borja@alumno.buap.mx>
2018-05-07 23:59:08 -0500

    cambio a ip del server

--- src/containers/Access/Access.jsx
+++ src/containers/Access/Access.jsx
@@ -54,7 +54,7 @@ class  Access extends Component{
             id_user: this.state.userId
         }
 
-        axios.post('http://localhost:3000/api/access',data)
+        axios.post('http://10.50.65.22:3000/api/access',data)
             .then((response) => {
                 this.props.history.replace('/all-access')
             })
